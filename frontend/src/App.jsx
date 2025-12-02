d684eabf5c7306271dbe345bc316500df4b403a7
vida-za <kuznetsov04.04@gmail.com>
2025-12-02 04:10:15 +0300

    front: Перешел на scss. Добавил домашнюю страницу

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import UsersPage from './pages/UsersPage'
-import './App.css'
+import HomePage from './pages/Home/HomePage'
+import './styles/App.scss'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/users" element={<UsersPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/pages/Home/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HomePage() {
+    return (
+        <div>
+            <main>
+                <h1>Главная страница</h1>
+            </main>
+        </div>
+    );
+}
